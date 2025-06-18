1abcafe9dc9fc6aff2f423d8da39594f02b5630e
blessed <ytminicraftyt@gmail.com>
2025-06-18 19:19:54 +0300

    first commit

---
+++ app/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    sqlalchemy_uri: str = "mysql+aiomysql://127.0.0.1:1234/db"
+    secret_key: str = "your_secret_key"
+    algorithm: str  = "HS256"
+    access_token_expire_minutes: int = 30
+
+
+settings = Settings()
